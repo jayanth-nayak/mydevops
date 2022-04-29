4e0c5ada8cb304c49680d6180604ad2ea9772742
jayanth-nayak <99711031+jayanth-nayak@users.noreply.github.com>
2022-04-29 11:24:36 +0530

    Update App.java

--- src/main/java/com/devopsclass/App.java
+++ src/main/java/com/devopsclass/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World THIS IS JAY NAYAK vajra.ai!" );
+        System.out.println( "Hello World THIS IS JAY NAYAK vajra.ai social!" );
     }
 } 
